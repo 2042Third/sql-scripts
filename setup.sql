087de3a30fa765ce80e279f91efa9c13a2a3c43a
mike yang <18604713262@163.com>
2021-12-20 16:05:10 -0500

    cmd script commit for sql-script

--- setup.sql
+++ setup.sql
@@ -1,2 +1,2 @@
 -- CREATE USER 'pdm-note'@localhost IDENTIFIED BY '56e80dd0b396bb9e26ee41efde5fdfd5518a400c9ffea101ff879feb6af623b8';
-GRANT ALL PRIVILEGES ON pdmdata.* TO pdm-note@localhost;
+GRANT ALL PRIVILEGES ON pdmdata.* TO 'pdm-note@localhost';
