6c22f605810059e0b2ec5dce7d4c211339871638
mike yang <18604713262@163.com>
2021-12-23 15:53:13 -0500

    cmd script commit for sql-script

--- set_file_track.sql
+++ set_file_track.sql
@@ -1,9 +1,9 @@
-CREATE USER 'pdm-files'@'localhost' IDENTIFIED BY '93eab1de880e03cc3f04f505298b3ab47034b784e3dd969d1f3d08f6978f0bb9';
-CREATE SCHEMA pdmfile;
+-- CREATE USER 'pdm-files'@'localhost' IDENTIFIED BY '93eab1de880e03cc3f04f505298b3ab47034b784e3dd969d1f3d08f6978f0bb9';
+CREATE or replace SCHEMA pdmfile;
 ALTER DATABASE pdmfile COLLATE = 'utf8_bin';
 GRANT ALL PRIVILEGES ON pdmfile.* TO 'pdm-files'@'localhost';
 use pdmfile;
-create table `fileservtracks` (
+create or replace table `fileservtracks` (
 `filename` VARCHAR(511) DEFAULT null,
 `action` VARCHAR(255) DEFAULT null,
 `time` VARCHAR(20) DEFAULT null,
