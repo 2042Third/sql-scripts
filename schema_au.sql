8c7b38ff96c39b510ee68191b19c068e12b8c26a
mike yang <18604713262@163.com>
2021-12-24 09:00:12 -0500

    cmd script commit for sql-script

--- schema_au.sql
+++ schema_au.sql
@@ -1,10 +1,10 @@
-CREATE USER 'pdm-auth'@'localhost' IDENTIFIED BY '3ff6f301706871c5e17d3d51c2c76c09c6c290d833eff70fce516aa2edc5f780';
-CREATE SCHEMA pdmauth;
-ALTER DATABASE pdmauth COLLATE = 'utf8_bin';
-GRANT ALL PRIVILEGES ON pdmauth.* TO 'pdm-auth'@'localhost';
+-- CREATE USER 'pdm-auth'@'localhost' IDENTIFIED BY '3ff6f301706871c5e17d3d51c2c76c09c6c290d833eff70fce516aa2edc5f780';
+-- CREATE SCHEMA pdmauth;
+-- ALTER DATABASE pdmauth COLLATE = 'utf8_bin';
+-- GRANT ALL PRIVILEGES ON pdmauth.* TO 'pdm-auth'@'localhost';
 use pdmauth;
 create table `users1` (
-`id` int IDENTITY(100, 1) PRIMARY KEY,
+`id` int SERIAL PRIMARY KEY,
 `username` VARCHAR(255) DEFAULT null,
 `pw` VARCHAR(511) DEFAULT null,
 `last-login` VARCHAR(255) DEFAULT null,
